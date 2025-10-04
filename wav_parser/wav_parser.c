3ecc92972e38c3b3376c0124b6b471c94a14b076
Myson Dio <sendorfbg@gmail.com>
2025-10-04 16:37:07 -0300

    idk

--- wav_parser/wav_parser.c
+++ wav_parser/wav_parser.c
@@ -52,7 +52,7 @@ static void read_text(char* buff, FILE* file) {
     buff[4] = '\0';
 }
 
-bool wav_parse_file(const char *filename, wav_file_t* wavFile)
+bool wav_parse_file(const char *filename, wav_file_t* wav_file)
 {
     bool retval = true;
     FILE* fp;
@@ -65,80 +65,80 @@ bool wav_parse_file(const char *filename, wav_file_t* wavFile)
         goto CLOSE_FILE;
     }
 
-    read_text(wavFile->header.RIFF, fp);
-    if(strcmp((wavFile->header.RIFF), "RIFF") != 0) {
-        fprintf(stderr,RED "[ERROR] - %s's first 4 bytes should be \"RIFF\" but are : %s\n" RESET, filename, wavFile->header.RIFF);
+    read_text(wav_file->header.RIFF, fp);
+    if(strcmp((wav_file->header.RIFF), "RIFF") != 0) {
+        fprintf(stderr,RED "[ERROR] - %s's first 4 bytes should be \"RIFF\" but are : %s\n" RESET, filename, wav_file->header.RIFF);
         retval = false;
         goto CLOSE_FILE;
     }
 
-    fread(&wavFile->header.file_size, 4/* bytes */, 1, fp);
+    fread(&wav_file->header.file_size, 4/* bytes */, 1, fp);
 
-    read_text(wavFile->header.WAVE, fp);
-    if(strcmp((wavFile->header.WAVE), "WAVE") != 0 ) {
-        fprintf(stderr,RED "[ERROR] - %s's 4 bytes should be \"WAVE\" but are : %s\n" RESET, filename, wavFile->header.WAVE);
+    read_text(wav_file->header.WAVE, fp);
+    if(strcmp((wav_file->header.WAVE), "WAVE") != 0 ) {
+        fprintf(stderr,RED "[ERROR] - %s's 4 bytes should be \"WAVE\" but are : %s\n" RESET, filename, wav_file->header.WAVE);
         retval = false;
         goto CLOSE_FILE;
     }
 
-    read_text(wavFile->header.fmt, fp);
-    if(strcmp((wavFile->header.fmt), "fmt ") != 0 ) {
-        fprintf(stderr,RED "[ERROR] - %s's 4 bytes should be \"fmt/0\" but are : %s\n" RESET, filename, wavFile->header.fmt);
+    read_text(wav_file->header.fmt, fp);
+    if(strcmp((wav_file->header.fmt), "fmt ") != 0 ) {
+        fprintf(stderr,RED "[ERROR] - %s's 4 bytes should be \"fmt/0\" but are : %s\n" RESET, filename, wav_file->header.fmt);
         retval = false;
         goto CLOSE_FILE;
     }
-    fread(&wavFile->header.chunk_size, 4/* bytes */, 1, fp);
-    fread(&wavFile->header.format_type, 2/* bytes */, 1, fp);
-    if(wavFile->header.format_type != 1) {
-        fprintf(stderr,RED "[ERROR] - %s's format type should be 1, but is : %d\n" RESET, filename, wavFile->header.format_type);
-        retval = false;
-        goto CLOSE_FILE;
-    }
-
-    fread(&wavFile->header.num_channels, 2/* bytes */, 1, fp);
-    fread(&wavFile->header.sample_rate, 4/* bytes */, 1, fp);
-    fread(&wavFile->header.byte_rate, 4/* bytes */, 1, fp);
-    fread(&wavFile->header.block_align, 2/* bytes */, 1, fp);
-    fread(&wavFile->header.bits_per_sample, 2/* bytes */, 1, fp);
-    if(wavFile->header.bits_per_sample != 16) {
-        fprintf(stderr,RED "[ERROR] - %s's bits per sample should be 16, but is : %d\n" RESET, filename, wavFile->header.sample_rate);
+    fread(&wav_file->header.chunk_size, 4/* bytes */, 1, fp);
+    fread(&wav_file->header.format_type, 2/* bytes */, 1, fp);
+    if(wav_file->header.format_type != 1) {
+        fprintf(stderr,RED "[ERROR] - %s's format type should be 1, but is : %d\n" RESET, filename, wav_file->header.format_type);
         retval = false;
         goto CLOSE_FILE;
     }
 
-    while (fread(wavFile->header.data, 1, 4, fp) == 4) {
+    fread(&wav_file->header.num_channels, 2/* bytes */, 1, fp);
+    fread(&wav_file->header.sample_rate, 4/* bytes */, 1, fp);
+    fread(&wav_file->header.byte_rate, 4/* bytes */, 1, fp);
+    fread(&wav_file->header.block_align, 2/* bytes */, 1, fp);
+    fread(&wav_file->header.bits_per_sample, 2/* bytes */, 1, fp);
+    if(wav_file->header.bits_per_sample != 16) {
+        fprintf(stderr,RED "[ERROR] - %s's bits per sample should be 16, but is : %d\n" RESET, filename, wav_file->header.sample_rate);
+        retval = false;
+        goto CLOSE_FILE;
+    }
+
+    while (fread(wav_file->header.data, 1, 4, fp) == 4) {
         int32_t chunkSize = 0;
         
         if (fread(&chunkSize, 1, 4, fp) != 4) {
-            fprintf(stderr, "Unexpected end of file while reading chunk size.\n");
+            fprintf(stderr, YELLOW "[WARNING] - Unexpected end of file while reading chunk size.\n" RESET);
             break;
         }
 
-        if (strncmp(wavFile->header.data, "data", 4) == 0) {
-            wavFile->header.data_size = chunkSize;
+        if (strncmp(wav_file->header.data, "data", 4) == 0) {
+            wav_file->header.data_size = chunkSize;
             break; 
         } else {
             // Skip over this chunk's data
             fseek(fp, chunkSize, SEEK_CUR);
         }
     }
-    wavFile->data_length = wavFile->header.data_size;
+    wav_file->data_length = wav_file->header.data_size;
 
-    wavFile->data = (uint8_t*)malloc(wavFile->data_length);
-    if(wavFile->data == NULL) {
-        fprintf(stderr,RED "[ERROR] - Failed to allocate %d bytes for data.\n" RESET, wavFile->data_length);
+    wav_file->data = (uint8_t*)malloc(wav_file->data_length);
+    if(wav_file->data == NULL) {
+        fprintf(stderr,RED "[ERROR] - Failed to allocate %d bytes for data.\n" RESET, wav_file->data_length);
         fprintf(stderr, YELLOW "[INFO] - Cause : %s.\n" RESET, strerror(errno));
         retval = false;
         goto CLOSE_FILE;
     }
 
-    if(fread(wavFile->data, 1, wavFile->data_length, fp) != wavFile->data_length) {
+    if(fread(wav_file->data, 1, wav_file->data_length, fp) != wav_file->data_length) {
         fprintf(stderr,RED "[ERROR] - Failed to read data's bytes.\n" RESET);
         retval = false;
         goto CLOSE_FILE;
     }
     
-    wavFile->samples = wavFile->data_length / wavFile->header.block_align;
+    wav_file->samples = wav_file->data_length / wav_file->header.block_align;
     fprintf(stdout, GREEN "\n[INFO] - %s parsed successfully!!!!\n\n" RESET, filename);
 CLOSE_FILE:
     fclose(fp);
