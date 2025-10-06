530fc31005bf5021ea3c60bd9392c54e2e23afef
Myson Dio <sendorfbg@gmail.com>
2025-10-05 23:49:35 -0300

    feat: adding new functions

--- win32/soundplayer.h
+++ win32/soundplayer.h
@@ -1,11 +1,12 @@
 #pragma once
-typedef wav_file_t wav;
+typedef struct state__ *state;
 
 typedef struct sound {
-    char* filename;
-    wav wav_file;
+    char* file_path;
+    state state;
 } sound;
 
-sound sound_init(const char* filename);
+sound sound_init(const char* file_path);
 void  sound_load(sound* _sound);
 void  sound_unload(sound* _sound);
+void  play_sound(sound* _sound);
