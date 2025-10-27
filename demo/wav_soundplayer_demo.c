2a9b6735ce7239fed14965b0a6d06e74037b4625
Myson Dio <sendorfbg@gmail.com>
2025-10-26 22:20:55 -0300

    feat: using sound* with sound_init in demo

--- demo/wav_soundplayer_demo.c
+++ demo/wav_soundplayer_demo.c
@@ -3,13 +3,13 @@
 
 int main(int argc, char const *argv[])
 {
-    sound snd = sound_init("resources/sound/bass-wiggle.wav");
-    sound_load(&snd);
-    play_sound(&snd);
-    playsound_ui_demo(&snd);
+    sound *snd = sound_init("resources/sound/bass-wiggle.wav");
+    sound_load(snd);
+    play_sound(snd);
+    playsound_ui_demo(snd);
     //?for replay demo purposes
-    play_sound(&snd);
-    playsound_ui_demo(&snd);
-    sound_unload(&snd);
+    play_sound(snd);
+    playsound_ui_demo(snd);
+    sound_unload(snd);
     return 0;
 }
